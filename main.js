e3e710904d62b06ef9d78d11163192610cd304ed
Harjot Singh <singhjot4185@gmail.com>
2024-09-19 11:09:15 +0530

    Vs code Landing page clone

---
+++ main.js
@@ -0,0 +1,14 @@
+function (arr){
+    const users = [{
+        name:"harjot",
+        age: 23,
+        gender: "male"
+    },{
+        name: "sonia",
+        age: 23,
+        gernder: "female"
+    },{
+        name: "rahul",
+        age: 15,
+        gender: "male"
+    }]
